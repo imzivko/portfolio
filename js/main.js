0009757b740174eeb167df3bbe112d4d1d34cae2
imzivko <imzivko@gmail.com>
2024-03-16 14:02:08 +0100

    revisit this

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ about.addEventListener("click", () => {
     title: "About Me",
     modal: window.mobileCheck() ? true : false,
     width: window.mobileCheck() ? "90%" : "30%",
-    height: window.mobileCheck() ? "90%" : "70%",
+    height: window.mobileCheck() ? "90%" : "50%",
     x: window.mobileCheck() ? "center" : "4%",
     y: window.mobileCheck() ? "center" : "4%",
     mount: aboutContent,
